3c45d51af00191f480e62374b730dec2bc2da2ac
mk <v.a.stekolschikov@gmail.com>
2023-09-12 22:28:19 +0300

    added toolkit example

--- redux-toolkit-rtk-query/src/store/reducers/ActionCreators.ts
+++ redux-toolkit-rtk-query/src/store/reducers/ActionCreators.ts
@@ -1,7 +1,5 @@
-import {AppDispatch} from "../store";
 import axios from "axios";
 import {IUser} from "../../models/IUser";
-import {userSlice} from "./UserSlice";
 import {createAsyncThunk} from "@reduxjs/toolkit";
 
 // export const fetchUsers = () => async (dispatch: AppDispatch) => {
@@ -18,10 +16,12 @@ export const fetchUsers = createAsyncThunk(
     'user/fetchAll',
     async (_, thunkAPI) => {
         try {
-            const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/user2s')
+            const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
             return response.data;
         } catch (e) {
             return thunkAPI.rejectWithValue("Не удалось загрузить пользователей")
         }
     }
 )
+
+
